0eac3bb89b4e98d221643be14f2341ff5dfe3c0d
Your Name <you@example.com>
2017-12-07 23:51:46 +0800

    asd

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,95 @@
+<footer>
+	<div class="top">
+		<div class="container top_o">
+			<div class="col-md-2 top_ot">
+				<img src="holder.js/100x100" alt="">
+				<h4 class="text-center"><b>值得信赖美妆电商</b></h4 class="text-center">
+				<p class="text-center">四千万用户信赖</p>
+			</div>
+			<div class="col-md-2 top_ot">
+				<img src="holder.js/100x100" alt="">
+				<h4 class="text-center"><b>值得信赖美妆电商</b></h4 class="text-center">
+				<p class="text-center">四千万用户信赖</p>
+			</div>
+			<div class="col-md-2 top_ot">
+				<img src="holder.js/100x100" alt="">
+				<h4 class="text-center"><b>值得信赖美妆电商</b></h4 class="text-center">
+				<p class="text-center">四千万用户信赖</p>
+			</div>
+			<div class="col-md-2 top_ot">
+				<img src="holder.js/100x100" alt="">
+				<h4 class="text-center"><b>值得信赖美妆电商</b></h4 class="text-center">
+				<p class="text-center">四千万用户信赖</p>
+			</div>
+			<div class="col-md-2 top_ot">
+				<img src="holder.js/100x100" alt="">
+				<h4 class="text-center"><b>值得信赖美妆电商</b></h4 class="text-center">
+				<p class="text-center">四千万用户信赖</p>
+			</div>
+			<div class="col-md-2 top_ot">
+				<img src="holder.js/100x100" alt="">
+				<h4 class="text-center"><b>值得信赖美妆电商</b></h4 class="text-center">
+				<p class="text-center">四千万用户信赖</p>
+			</div>
+		</div>
+
+		<div class="container top_t">
+			<div class="col-md-2 top_tt">
+				<h5>服务保障</h5>
+				<ul>	
+					<li>品牌真品防伪码</li>
+					<li>正品保证</li>
+					<li>30天无条件退货</li>
+					<li>7×24小时客服服务</li>
+					<li>总裁邮箱ceo@jumei.com</li>
+				</ul>
+			</div>
+			<div class="col-md-2 top_tt">
+				<h5>服务保障</h5>
+				<ul>	
+					<li>品牌真品防伪码</li>
+					<li>正品保证</li>
+					<li>30天无条件退货</li>
+					<li>7×24小时客服服务</li>
+					<li>总裁邮箱ceo@jumei.com</li>
+				</ul>
+			</div>
+			<div class="col-md-2 top_tt">
+				<h5>服务保障</h5>
+				<ul>	
+					<li>品牌真品防伪码</li>
+					<li>正品保证</li>
+					<li>30天无条件退货</li>
+					<li>7×24小时客服服务</li>
+					<li>总裁邮箱ceo@jumei.com</li>
+				</ul>
+			</div>
+			<div class="col-md-2 top_tt">
+				<h5>服务保障</h5>
+				<ul>	
+					<li>品牌真品防伪码</li>
+					<li>正品保证</li>
+					<li>30天无条件退货</li>
+					<li>7×24小时客服服务</li>
+					<li>总裁邮箱ceo@jumei.com</li>
+				</ul>
+			</div>
+			<div class="col-md-2 top_tt">
+					<h5>手机聚美</h5>
+					<img src="holder.js/80x80" alt="">
+					<p>下载移动客户端聚美微信</p>
+			</div>
+			<div class="col-md-2 top_tt">
+					<h5>手机聚美</h5>
+					<img src="holder.js/80x80" alt="">
+					<p>下载移动客户端聚美微信</p>
+			</div>
+
+		</div>
+		<div class="wokao">
+			<img src="./bootstrap/images/28.jpg" width="100%" height="100%" alt="">
+		</div>
+		
+	</div>
+	<div class="buttom" style="background-color: red;"></div>
+</footer>
