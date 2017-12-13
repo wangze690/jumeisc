501bc550185104b2beeabc0ddf045a1dc04665a8
xubaoin <1188523857@qq.com>
2017-12-13 11:12:40 +0800

    jumei

--- app/Http/Controllers/ShoucangController.php
+++ app/Http/Controllers/ShoucangController.php
@@ -11,7 +11,14 @@ class ShoucangController extends Controller
     {
     	$nav = DB::table('nav')->where('path',2)->get();
     	$shoucang = DB::table('shoucang')->get();
-    	
+    	foreach ($shoucang as $key => $value) {
+    	}
+    	$sp_id = $value->sp_id;
+    	$user_id = $value->user_id;
+
+    	$user = DB::table('users')->where('id',$user_id)->get();
+    	$sp_id = DB::table('shop')->where('id',$sp_id)->get();
+    	dd($sp_id);
 
     	return view('shoucang.shoucang',[
 
